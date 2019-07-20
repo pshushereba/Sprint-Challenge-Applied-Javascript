a59fa3dbf0f3f050f6e50dd7ae6212dbea84ceeb
Patrick Shushereba <pshushereba@gmail.com>
2019-07-20 11:46:01 -0700

    Finished Cards Section.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,50 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((axiosData) => {
+    createCard(axiosData.data.articles);
+  })
+  .catch((err) => {
+    console.log("There was an error", err);
+  });
+
+function createCard(articleData) {    
+    
+    for (let category in articleData) {
+        const categoryArr = articleData[category];
+
+        categoryArr.forEach((article) => {
+            const cardDiv = document.createElement('div');
+            cardDiv.classList.add('card');
+    
+            const headlineDiv = document.createElement('div');
+            headlineDiv.classList.add('headline');
+            headlineDiv.textContent = article.headline;
+            cardDiv.appendChild(headlineDiv);
+    
+            const authorDiv = document.createElement('div');
+            authorDiv.classList.add('author');
+            cardDiv.appendChild(authorDiv);
+
+            const imgDiv = document.createElement('div');
+            imgDiv.classList.add('img-container');
+            authorDiv.appendChild(imgDiv);
+
+            const authorImg = document.createElement('img');
+            authorImg.src = article.authorPhoto;
+            imgDiv.appendChild(authorImg);
+
+            const authorSpan = document.createElement('span');
+            authorSpan.textContent = article.authorName;
+            authorDiv.appendChild(authorSpan);
+        
+            articleContainer.appendChild(cardDiv);    
+        });
+    }
+}
+
+
+const articleContainer = document.querySelector(".cards-container");
